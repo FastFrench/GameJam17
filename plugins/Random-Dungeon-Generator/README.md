38ea80e266c2cde64ab54690d1c60014a5e38779
FastFrench <antispam@laposte.net>
2017-10-27 13:12:10 +0200

    Gathering some materials

---
+++ plugins/Random-Dungeon-Generator/README.md
@@ -0,0 +1,32 @@
+This Plugin is based on the great work of BigBadWofl (http://bigbadwofl.me/).
+
+What does it do?
+================
+This Plugin generates a random Dungeon and fills an Array with the data.
+Each x/y coordinate is filled with the following information:
+1=Floor
+2=Wall
+3=Empty
+
+Roadmap
+=======
+v1
+--
+- Room and Path Generation
+
+v2
+--
+- instance variable to return status of level generation
+
+Planned
+=======
+v3
+--
+- Generation of locked doors and keys
+- Entry- / Exitlocation
+
+ETA tbc
+-------
+- Placement of Monsters
+- improved room creation algorithm
+- seed based level generation
---
+++ plugins/Random-Dungeon-Generator/runtime.js
@@ -0,0 +1,346 @@
+// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.UBIRDG = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.UBIRDG.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+
+		// any other properties you need, e.g...
+		// this.myValue = 0;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		// note the object is sealed after this call; ensure any properties you'll ever need are set on the object
+		// e.g...
+		// this.myValue = 0;
+		cr.plugins_.UBIRDG.prototype.loaded = 0;
+	};
+	
+	// called whenever an instance is destroyed
+	// note the runtime may keep the object after this call for recycling; be sure
+	// to release/recycle/reset any references to other objects in this function.
+	instanceProto.onDestroy = function ()
+	{
+	};
+	
+	// called when saving the full state of the game
+	instanceProto.saveToJSON = function ()
+	{
+		// return a Javascript object containing information about your object's state
+		// note you MUST use double-quote syntax (e.g. "property": value) to prevent
+		// Closure Compiler renaming and breaking the save format
+		return {
+			// e.g.
+			//"myValue": this.myValue
+		};
+	};
+	
+	// called when loading the full state of the game
+	instanceProto.loadFromJSON = function (o)
+	{
+		// load from the state previously saved by saveToJSON
+		// 'o' provides the same object that you saved, e.g.
+		// this.myValue = o["myValue"];
+		// note you MUST use double-quote syntax (e.g. o["property"]) to prevent
+		// Closure Compiler renaming and breaking the save format
+	};
+	
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw)
+	{
+	};
+	
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		// "properties" is an array of individual debugger properties to display
+		// with their name and value, and some other optional settings.
+		propsections.push({
+			"title": "My debugger section",
+			"properties": [
+				// Each property entry can use the following values:
+				// "name" (required): name of the property (must be unique within this section)
+				// "value" (required): a boolean, number or string for the value
+				// "html" (optional, default false): set to true to interpret the name and value
+				//									 as HTML strings rather than simple plain text
+				// "readonly" (optional, default false): set to true to disable editing the property
+				
+				// Example:
+				// {"name": "My property", "value": this.myValue}
+			]
+		});
+	};
+	
+	instanceProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Called when a non-readonly property has been edited in the debugger. Usually you only
+		// will need 'name' (the property name) and 'value', but you can also use 'header' (the
+		// header title for the section) to distinguish properties with the same name.
+		if (name === "My property")
+			this.myProperty = value;
+	};
+	/**END-PREVIEWONLY**/
+
+var Dungeon = {
+    map: null,
+    rooms: [],
+    Generate: function (MinRoom, MaxRoom, MinSize, MaxSize, MapSize, Squash) {
+        this.map = [];
+        cr.plugins_.UBIRDG.prototype.loaded = 0;
+        for (var x = 0; x < MapSize; x++) {
+            this.map[x] = [];
+            for (var y = 0; y < MapSize; y++) {
+                this.map[x][y] = 0;
+            }
+        }
+
+        var room_count = Helpers.GetRandom(MinRoom, MaxRoom);
+        var maxLoop = room_count * 2;
+        for (var i = 0; i < room_count && maxLoop>0; i++ , maxLoop--) {
+            var room = {};
+            room.x = Helpers.GetRandom(1, MapSize - MaxSize - 1);
+            room.y = Helpers.GetRandom(1, MapSize - MaxSize - 1);
+            room.w = Helpers.GetRandom(MinSize, MaxSize);
+            room.h = Helpers.GetRandom(MinSize, MaxSize);
+
+            if (this.DoesCollide(room)) {
+                i--;
+                continue;
+            }
+            room.w--;
+            room.h--;
+
+            this.rooms.push(room);
+        }
+	if (Squash == 0)
+	{
+        	this.SquashRooms();
+	}
+
+        for (i = 0; i < room_count; i++) {
+            var roomA = this.rooms[i];
+            var roomB = this.FindClosestRoom(roomA);
+
+            var pointA = {
+                x: Helpers.GetRandom(roomA.x, roomA.x + roomA.w),
+                y: Helpers.GetRandom(roomA.y, roomA.y + roomA.h)
+            };
+            var pointB = {
+                x: Helpers.GetRandom(roomB.x, roomB.x + roomB.w),
+                y: Helpers.GetRandom(roomB.y, roomB.y + roomB.h)
+            };
+
+            while ((pointB.x != pointA.x) || (pointB.y != pointA.y)) {
+                if (pointB.x != pointA.x) {
+                    if (pointB.x > pointA.x) pointB.x--;
+                    else pointB.x++;
+                } else if (pointB.y != pointA.y) {
+                    if (pointB.y > pointA.y) pointB.y--;
+                    else pointB.y++;
+                }
+
+                this.map[pointB.x][pointB.y] = 1;
+            }
+        }
+
+        for (i = 0; i < room_count; i++) {
+            var room = this.rooms[i];
+            for (var x = room.x; x < room.x + room.w; x++) {
+                for (var y = room.y; y < room.y + room.h; y++) {
+                    this.map[x][y] = 1;
+                }
+            }
+        }
+
+        for (var x = 0; x < MapSize; x++) {
+            for (var y = 0; y < MapSize; y++) {
+                if (this.map[x][y] == 1) {
+                    for (var xx = x - 1; xx <= x + 1; xx++) {
+                        for (var yy = y - 1; yy <= y + 1; yy++) {
+                            if (this.map[xx][yy] == 0) this.map[xx][yy] = 2;
+                        }
+                    }
+                }
+            }
+        }
+        cr.plugins_.UBIRDG.prototype.loaded = 1;
+       return this.map;
+    },
+    FindClosestRoom: function (room) {
+        var mid = {
+            x: room.x + (room.w / 2),
+            y: room.y + (room.h / 2)
+        };
+        var closest = null;
+        var closest_distance = 1000;
+        for (var i = 0; i < this.rooms.length; i++) {
+            var check = this.rooms[i];
+            if (check == room) continue;
+            var check_mid = {
+                x: check.x + (check.w / 2),
+                y: check.y + (check.h / 2)
+            };
+            var distance = Math.min(Math.abs(mid.x - check_mid.x) - (room.w / 2) - (check.w / 2), Math.abs(mid.y - check_mid.y) - (room.h / 2) - (check.h / 2));
+            if (distance < closest_distance) {
+                closest_distance = distance;
+                closest = check;
+            }
+        }
+        return closest;
+    },
+    SquashRooms: function () {
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < this.rooms.length; j++) {
+                var room = this.rooms[j];
+                while (true) {
+                    var old_position = {
+                        x: room.x,
+                        y: room.y
+                    };
+                    if (room.x > 1) room.x--;
+                    if (room.y > 1) room.y--;
+                    if ((room.x == 1) && (room.y == 1)) break;
+                    if (this.DoesCollide(room, j)) {
+                        room.x = old_position.x;
+                        room.y = old_position.y;
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    DoesCollide: function (room, ignore) {
+        for (var i = 0; i < this.rooms.length; i++) {
+            if (i == ignore) continue;
+            var check = this.rooms[i];
+            if (!((room.x + room.w < check.x) || (room.x > check.x + check.w) || (room.y + room.h < check.y) || (room.y > check.y + check.h))) return true;
+        }
+
+        return false;
+    }
+};
+
+
+var Helpers = {
+    GetRandom: function (low, high) {
+        return~~ (Math.random() * (high - low)) + low;
+    }
+};
+
+
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	// the example condition
+	Cnds.prototype.MyCondition = function (myparam)
+	{
+		// return true if number is positive
+		return myparam >= 0;
+	};
+	
+	// ... other conditions here ...
+	
+	pluginProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	// the example action
+	Acts.prototype.createDungeon = function (MinRooms, MaxRooms, MinSize, MaxSize, Size, squash, array_objs)
+	{
+	    	assert2(cr.plugins_.Arr, "[DungeonGenerator] Error:No Array object found.");
+	    	
+        	var array_obj = array_objs.getFirstPicked();
+        	var is_array_inst = (array_obj instanceof cr.plugins_.Arr.prototype.Instance);
+        	assert2(is_array_inst, "[DungeonGenerator] Error:Need an array object.");
+        	
+        	//prepare array to be filled with data
+        	cr.plugins_.Arr.prototype.acts.SetSize.apply(array_obj, [Size, Size, 1]);
+        	
+		Dungeon.Generate(MinRooms, MaxRooms, MinSize, MaxSize, Size, squash);
+        	var array_set = cr.plugins_.Arr.prototype.acts.SetXYZ;
+        	for (var x=0; x<Size;x++)
+        	{
+        		for (var z=0; z<Size;z++)	
+        		{
+        			array_set.apply(array_obj, [x, z, 0, Dungeon.map[x][z]])
+               		}
+        	}
+	};
+	
+	// ... other actions here ...
+	
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {
+		Exps.prototype.LoadStatus = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+		{
+			var set = (cr.plugins_.UBIRDG.prototype.loaded);
+			ret.set_int(set);				// return our value
+		};
+
+	};
+	
+
+	// ... other expressions here ...
+	
+	pluginProto.exps = new Exps();
+
+}());
